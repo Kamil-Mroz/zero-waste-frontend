de318954f8f39d1443b3cd9c52a84247ace701f6
Kamil-Mroz <kamilmp17@gmail.com>
2026-07-31 15:26:40 +0200

    update to vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,14 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), "");
 
 	return {
+		resolve: { tsconfigPaths: true },
 		plugins: [
 			devtools(),
-			tsconfigPaths({ projects: ["./tsconfig.json"] }),
 			tailwindcss(),
 			tanstackRouter({ target: "react", autoCodeSplitting: true }),
 			viteReact(),
