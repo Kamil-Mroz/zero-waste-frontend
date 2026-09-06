fed6deda2751e48031e58e1a923c94a7f2d4f5cf
Kamil-Mroz <kamilmp17@gmail.com>
2026-09-06 14:24:37 +0200

    fix: remove error message when logout in a authenticated route

--- src/features/auth/hooks/mutation-options.tsx
+++ src/features/auth/hooks/mutation-options.tsx
@@ -81,14 +81,12 @@ export function useLogoutMutation() {
 	const { logout } = useAuth();
 	const navigate = useNavigate();
 	const queryClient = useQueryClient();
-	const router = useRouter();
 
 	return useMutation({
 		mutationFn: logout,
 		onSuccess: async () => {
 			queryClient.clear();
-			await router.invalidate();
-			await navigate({ to: "/login" });
+			await navigate({ to: "/marketplace" });
 			if (isMobile) toggleSidebar();
 		},
 	});
